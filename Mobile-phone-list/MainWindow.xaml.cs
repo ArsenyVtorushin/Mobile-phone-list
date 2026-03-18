9360c3e1b69c3ee525741b9825a7589cc773005e
Top <Top@WIN-9BH2USJ2P08>
2026-03-18 19:48:17 +0500

    Добавьте файлы проекта.

---
+++ Mobile-phone-list/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mobile_phone_list
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Phone> phones;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            phones = new List<Phone>()
+            {
+                new Phone {Company = "Apple", Title = "IPhone 10", Price = 58000},
+                new Phone {Company = "Samsung", Title = "Galaxy S10", Price = 65000},
+                new Phone {Company = "Google", Title = "Pixel 4", Price = 50000},
+                new Phone {Company = "OnePlus", Title = "OnePlus 7T", Price = 45000}
+            };
+
+            mainListBox.ItemsSource = phones;
+            
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            phones.Add(new Phone
+            {
+                Title = titleView.Text,
+                Company = companyView.Text,
+                Price = Convert.ToDecimal(priceView.Text)
+            });
+
+            mainListBox.ItemsSource = null;
+            mainListBox.ItemsSource = phones;
+
+            titleView.Text = null;
+            companyView.Text = null;
+            priceView.Text = null;
+        }
+
+        private void RemoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            phones.Remove(mainListBox.SelectedItem as Phone);
+            mainListBox.ItemsSource = null;
+            mainListBox.ItemsSource = phones;
+        }
+    }
+}
---
+++ Mobile-phone-list/Phone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobile_phone_list
+{
+    public class Phone
+    {
+        public string Title { get; set; }
+        public string Company { get; set; }
+        public decimal Price { get; set; }
+    }
+}
